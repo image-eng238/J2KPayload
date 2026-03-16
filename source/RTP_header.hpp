28eaffb373ef592365e15fa8b49d217370c35bd5
taketo <228238@st.takushoku-u.ac.jp>
2026-03-16 16:28:17 +0900

    test rtp & j2kpayload

---
+++ source/RTP_header.hpp
@@ -0,0 +1,67 @@
+#pragma once
+
+#include <cstdint>
+#include <cstddef>
+
+class RTPHeader {
+public:
+    RTPHeader() = default;
+    RTPHeader(const uint8_t* const ptr) : pointer{ptr} {}
+
+    uint8_t get_V() const { return pointer[0] & 0xC0; };                                                           // version: 2 bits 0b10で固定
+    uint8_t get_P() const { return pointer[0] & 0x20; };                                                           // padding: 1 bit
+    uint8_t get_X() const { return pointer[0] & 0x10; };                                                           // extension: 1 bit
+    uint8_t get_CC() const { return pointer[0] & 0x0F; };                                                          // CSRC_count: 4 bits
+    uint8_t get_M() const { return pointer[1] & 0x80; };                                                           // marker: 1 bit
+    uint8_t get_PT() const { return pointer[1] & 0x7F; };                                                          // payload_type: 7 bits
+    uint16_t get_sequence_number() const { return pointer[2] << 8 | pointer[3]; };                                 // 16 bits
+    uint32_t get_timestamp() const { return pointer[4] << 24 | pointer[5] << 16 | pointer[6] << 8 | pointer[7]; }; // 32 bits
+    uint32_t get_SSRC() const { return pointer[8] << 24 | pointer[9] << 16 | pointer[10] << 8 | pointer[11]; };    // 32 bits
+    uint8_t get_header_length() const { return length; }
+
+private:
+    const uint8_t* pointer;
+    static constexpr uint8_t length = 12;
+};
+
+class J2KPayloadHeader {
+public:
+    J2KPayloadHeader() = default;
+    J2KPayloadHeader(const uint8_t* const ptr) : pointer{ptr} {}
+
+    uint8_t get_MH() const { return pointer[0] & 0xC0; }                           // Codestream Main Header Presence: 2 bits
+    uint8_t get_TP() const { return pointer[0] & 0x38; }                           // Image Type: 3 bits
+    uint16_t get_PTSTAMP() const { return (pointer[1] & 0x0F) << 8 | pointer[2]; } // Precision Timestamp: 12 bits
+    uint8_t get_ESEQ() const { return pointer[3]; }                                // Extended Sequence Number High-Order Bits: 8 bits
+    uint8_t get_header_length() const { return length; }
+
+    // main
+    uint8_t get_main_ORDH() const { return pointer[0] & 0x07; }  // Progression Order Flag, Main Packet: 3 bits
+    uint8_t get_main_P() const { return pointer[1] & 0x80; }     // Precision Timestamp Presence: 1 bit
+    uint8_t get_main_XTRAC() const { return pointer[1] & 0x70; } // Extension Payload Length: 3 bits
+    uint16_t get_main_PTSTAMP() const { return get_PTSTAMP(); }  // Precision Timestamp: 12 bits
+    uint8_t get_main_ESEQ() const { return get_ESEQ(); }         // Extended Sequence Number High-Order Bits: 8 bits
+
+    uint8_t get_main_R() const { return pointer[4] & 0x80; }     // Codestream Main Header Reuse: 1 bit
+    uint8_t get_main_S() const { return pointer[4] & 0x40; }     // Parameterized Colorspace Presence: 1 bit
+    uint8_t get_main_C() const { return pointer[4] & 0x20; }     // Code-Block Caching Usage: 1 bit
+    uint8_t get_main_RSVD() const { return pointer[4] & 0x1E; }  // Reserved: 4 bits
+    uint8_t get_main_RANGE() const { return pointer[4] & 0x01; } // Video Full Range Usage: 1 bit
+    uint8_t get_main_PRIMS() const { return pointer[5]; }        // Color Primaries: 8 bits
+    uint8_t get_main_TRANS() const { return pointer[6]; }        // Transfer Characteristice: 8 bits
+    uint8_t get_main_MAT() const { return pointer[7]; }          // Colo Matrix Coefficients: 8 bits
+
+    // body
+    uint8_t get_body_RES() const { return pointer[0] & 0x07; }  // Resolution Levels: 3 bits
+    uint8_t get_body_ORDB() const { return pointer[1] & 0x80; } // Progression Order Flag, Body Packet: 1 bit is_resync_point
+    uint8_t get_body_QUAL() const { return pointer[1] & 0x70; } // Quality Layers: 3 bit
+    uint16_t get_body_PTSTAMP() const { return get_PTSTAMP(); } // Precision Timestamp: 12 bits
+    uint8_t get_body_ESEQ() const { return get_ESEQ(); }        // Extended Sequence Number High-Order Bits: 8 bits
+
+    uint16_t get_body_POS() const { return (pointer[4] << 4) | ((pointer[5] & 0xF0) >> 4); }         // Resyns Point Offset: 12 bits
+    uint32_t get_body_PID() const { return pointer[5] & 0x0F << 16 | pointer[6] << 8 | pointer[7]; } // Precinct Identifier: 20 bits
+
+private:
+    const uint8_t* pointer;
+    static constexpr uint8_t length = 8;
+};
--- source/UDP.hpp
+++ source/UDP.hpp
@@ -9,19 +9,19 @@
 class UDPBase {
 public:
     UDPBase() {
-        sock = socket(AF_INET, SOCK_DGRAM, 0);
-        if (sock == -1) {
-            std::cout << "socket error" << std::endl;
-            exit(1);
-        }
+        // sock = socket(AF_INET, SOCK_DGRAM, 0);
+        // if (sock == -1) {
+        //     std::cout << "socket error" << std::endl;
+        //     exit(1);
+        // }
 
-        socket_address = []() constexpr {
-            sockaddr_in out{};
-            out.sin_family      = AF_INET;
-            out.sin_port        = htons(50001);
-            out.sin_addr.s_addr = inet_addr("127.0.0.1");
-            return out;
-        }();
+        // socket_address = []() constexpr {
+        //     sockaddr_in out{};
+        //     out.sin_family      = AF_INET;
+        //     out.sin_port        = htons(50001);
+        //     out.sin_addr.s_addr = inet_addr("127.0.0.1");
+        //     return out;
+        // }();
     }
     ~UDPBase() {
         if (sock != -1) close(sock);
@@ -46,11 +46,30 @@ public:
 
 class UDPReceiver : public UDPBase {
 public:
-    UDPReceiver() {
-        if (bind(sock, reinterpret_cast<const sockaddr*>(&socket_address), sizeof(sockaddr_in)) == -1) {
-            std::cout << "bind error" << std::endl;
+    UDPReceiver() {}
+    UDPReceiver(const char* const address, const uint16_t port) {
+        if (!sock_bind(address, port)) {
             exit(1);
         }
+    }
+    bool sock_bind(const char* const address = "127.0.0.1", const uint16_t port = 50001) {
+        sock = socket(AF_INET, SOCK_DGRAM, 0);
+        if (sock == -1) {
+            std::cout << "socket error" << std::endl;
+            return false;
+        }
+        socket_address = [&]() {
+            sockaddr_in out{};
+            out.sin_family      = AF_INET;
+            out.sin_port        = htons(port);
+            out.sin_addr.s_addr = inet_addr(address);
+            return out;
+        }();
+        if (bind(sock, reinterpret_cast<const sockaddr*>(&socket_address), sizeof(sockaddr_in)) == -1) {
+            std::cout << "bind error" << std::endl;
+            return false;
+        }
+        return true;
     }
     int receive(void* const buf_ptr, const size_t buf_size) {
         auto output = recv(sock, buf_ptr, buf_size, 0);
