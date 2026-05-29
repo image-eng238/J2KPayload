43902e717390b56561e11156679e262c874fe8db
taketo <26m206@st.takushoku-u.ac.jp>
2026-05-29 13:33:03 +0900

    RTPsender: add option --Enter

--- RTPsender.cpp
+++ RTPsender.cpp
@@ -1,6 +1,7 @@
 #include "UDP.hpp"
 #include "RTP_file_format.hpp"
 #include "RTP_header.hpp"
+#include "argument.hpp"
 
 #include <vector>
 #include <string>
@@ -20,50 +21,68 @@ int main(int argc, char** argv) {
         arg_view[i - 1] = argv[i];
     }
 
-    std::string_view addr, rtp_path;
-    uint16_t port          = 0;
+    std::string_view addr  = "127.0.0.1", rtp_path;
+    uint16_t port          = 50001;
     int64_t interval       = 0;
     size_t skep_frame      = 0;
     int64_t allowable_time = 16;
     size_t file_loop       = 0;
+    bool is_enter          = false;
 
-    for (auto it = arg_view.begin(); it != arg_view.end(); ++it) {
-        if (*it == "-a") {
-            it++;
-            addr = it->data();
-        }
-        if (*it == "-p") {
-            it++;
-            if (std::from_chars(it->begin(), it->end(), port).ptr != it->end()) {
-                port = 0;
-            }
-        }
-        if (*it == "-r") {
-            it++;
-            rtp_path = it->data();
-        }
-        if (*it == "-i") {
-            it++;
-            if (std::from_chars(it->begin(), it->end(), interval).ptr != it->end()) {
-                interval = 0;
-            }
-        }
-        if (*it == "-s") {
-            it++;
-            if (std::from_chars(it->begin(), it->end(), skep_frame).ptr != it->end()) {
-                skep_frame = 0;
-            }
-        }
-        if (*it == "-f") {
-            it++;
-            if (std::from_chars(it->begin(), it->end(), allowable_time).ptr != it->end()) {
-                allowable_time = 16;
-            }
-        }
-        if (*it == "-l") {
-            it++;
-            if (std::from_chars(it->begin(), it->end(), file_loop).ptr != it->end()) {
-                file_loop = 0;
+    {
+        using namespace tklib;
+        static constexpr argument_list<9> args_list(
+            {{'a', "address", "Send to IPv4 address. default: 127.0.0.1"},
+             {'p', "port", "Send to port. default: 50001"},
+             {'r', "rtp_file", "The .rtp file source of packet to send"},
+             {'i', "interval", "Packet transmission interval (microseconds)"},
+             {'s', "skep_frame", "Number of frames to skip"},
+             {'f', "frame_rate", "Frame fate. default: 60"},
+             {'l', "loop", "Number of loops"},
+             {0, "Enter", "Send with Enter key"},
+             {'h', "help", "Show this"}}
+        );
+        static_assert(args_list.check());
+        argument_t args(argc, argv, args_list);
+        while (!args.empty()) {
+            switch (args.get_opt()) {
+                case args_list('a'):
+                    addr = args.pop();
+                    break;
+                case args_list('p'): {
+                    const auto tmp = args.pop();
+                    std::from_chars(tmp.begin(), tmp.end(), port);
+                } break;
+                case args_list('r'):
+                    rtp_path = args.pop();
+                    break;
+                case args_list('i'): {
+                    const auto tmp = args.pop();
+                    std::from_chars(tmp.begin(), tmp.end(), interval);
+                } break;
+                case args_list('s'): {
+                    const auto tmp = args.pop();
+                    std::from_chars(tmp.begin(), tmp.end(), skep_frame);
+                } break;
+                case args_list('f'): {
+                    const auto tmp_s = args.pop();
+                    int64_t tmp_i;
+                    std::from_chars(tmp_s.begin(), tmp_s.end(), tmp_i);
+                    allowable_time = static_cast<int64_t>(1.0 / tmp_i * 1000);
+                } break;
+                case args_list('l'): {
+                    const auto tmp = args.pop();
+                    std::from_chars(tmp.begin(), tmp.end(), file_loop);
+                } break;
+                case args_list("Enter"):
+                    is_enter = true;
+                    break;
+                case args_list('h'):
+                    args_list.print_arg();
+                    exit(0);
+                default:
+                    fprintf(stderr, "unknown argument: %s\n", args.show().data());
+                    exit(1);
             }
         }
     }
@@ -99,9 +118,15 @@ int main(int argc, char** argv) {
                 send_pktsize = rtp.get_packet(send_buffer, diff_seq * current_file_loop);
             }
         }
-        udp.send(send_buffer, send_pktsize);
         sended_packet++;
         now_frame++;
+        if (is_enter) {
+            getc(stdin);
+            std::cout << "send main packet, number of sent packets: " << sended_packet << std::endl;
+            udp.send(send_buffer, send_pktsize);
+        } else {
+            udp.send(send_buffer, send_pktsize);
+        }
 
         uint32_t sequence_number = (J2KPayloadHeader(RTPHeader(send_buffer).get_header_length() + send_buffer).get_ESEQ() << 16) | RTPHeader(send_buffer).get_sequence_number();
         if (!((sequence_number == pre_sequence_number + 1) || (pre_sequence_number == 0) || (sequence_number == 0))) {
@@ -114,8 +139,14 @@ int main(int argc, char** argv) {
         assert(J2KPayloadHeader(RTPHeader(send_buffer).get_header_length() + send_buffer).get_MH());
         while (true) { // body packet
             send_pktsize = rtp.get_packet(send_buffer, diff_seq * current_file_loop);
-            udp.send(send_buffer, send_pktsize);
             sended_packet++;
+            if (is_enter) {
+                getc(stdin);
+                std::cout << "send body packet, number of set packets: " << sended_packet << std::endl;
+                udp.send(send_buffer, send_pktsize);
+            } else {
+                udp.send(send_buffer, send_pktsize);
+            }
             // std::this_thread::sleep_for(std::chrono::milliseconds(interval));
             std::this_thread::sleep_for(std::chrono::microseconds(interval));
 
