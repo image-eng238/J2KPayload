a3e50e603f39622ee289f34b447cfe24048c82f1
taketo <228238@st.takushoku-u.ac.jp>
2026-06-29 00:52:12 +0900

    fixed mum_value_v

--- source/wrap_number.hpp
+++ source/wrap_number.hpp
@@ -3,12 +3,17 @@
 #include <cstdint>
 
 template <typename T>
-constexpr auto maximum_value_v = static_cast<T>(~T{} > 0 ? ~T{} : ~T{} ^ 1 << sizeof(T) * 8 - 1);
+constexpr auto maximum_value_v = static_cast<T>(static_cast<T>(~T{}) > 0 ? ~T{} : ~T{} ^ static_cast<T>(1) << sizeof(T) * 8 - 1);
 template <typename T>
-constexpr auto minimum_value_v = static_cast<T>(~T{} > 0 ? T{} : 1 << sizeof(T) * 8 - 1);
+constexpr auto minimum_value_v = static_cast<T>(static_cast<T>(~T{}) > 0 ? T{} : static_cast<T>(1) << sizeof(T) * 8 - 1);
 static_assert(maximum_value_v<int8_t> == INT8_MAX && minimum_value_v<int8_t> == INT8_MIN);
+static_assert(maximum_value_v<uint8_t> == UINT8_MAX && minimum_value_v<uint8_t> == 0);
+static_assert(maximum_value_v<int16_t> == INT16_MAX && minimum_value_v<int16_t> == INT16_MIN);
+static_assert(maximum_value_v<uint16_t> == UINT16_MAX && minimum_value_v<uint16_t> == 0);
 static_assert(maximum_value_v<int32_t> == INT32_MAX && minimum_value_v<int32_t> == INT32_MIN);
 static_assert(maximum_value_v<uint32_t> == UINT32_MAX && minimum_value_v<uint32_t> == 0);
+static_assert(maximum_value_v<int64_t> == INT64_MAX && minimum_value_v<int64_t> == INT64_MIN);
+static_assert(maximum_value_v<uint64_t> == UINT64_MAX && minimum_value_v<uint64_t> == 0);
 
 template <size_t N>
 constexpr bool is_pow2_v = [](size_t n = N) { for (; !(n & 1); n >>= 1); return !(n & SIZE_MAX - 1); }();
@@ -50,7 +55,7 @@ public:
         value--;
         return tmp;
     }
-    constexpr explicit operator value_type() {
+    constexpr operator value_type() {
         return get();
     }
 
